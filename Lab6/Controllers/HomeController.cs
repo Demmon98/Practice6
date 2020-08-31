d5a9a09a1dff3afa444320075c124938502df300
Demmon <dima.521.shvab@gmail.com>
2020-08-31 14:00:20 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Lab6.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Entities model = new Entities();
+
+            return View(model);
+        }
+    }
+}
